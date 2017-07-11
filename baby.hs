04d56ff1a2b3a0ac1cef1e8a4bd924189721b260
sakamoto <sakamoto@infoscience.co.jp>
2017-07-11 18:08:45 +0900

    mod

--- baby.hs
+++ baby.hs
@@ -5,3 +5,18 @@ doubleSmallNumber x = if x > 100
                        else x*2
 doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
 a = 1
+
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumfence' :: Double -> Double
+circumfence' r = 2 * pi * r
